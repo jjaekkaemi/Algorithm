8c53aaefc899dcb1a12e92aaffd13d118ae0330d
jjaekkaemi <35017054+jjaekkaemi@users.noreply.github.com>
2019-01-04 00:32:46 +0900

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <queue> 
+
+using namespace std;
+
+int main(void) {
+	queue<int> q ;
+	q.push(7) ;
+	q.push(4) ;
+	q.push(3);
+	q.pop() ;
+	while(!q.empty()) {
+		cout<<q.front() << ' ' ;
+		q.pop();
+	}
+	return 0 ;
+}
